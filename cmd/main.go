3ab8b5dcc6573f38acca3aa249f213726c56fdd0
Bilgihan Kose <bilgihankose@gmail.com>
2022-01-23 13:15:34 +0300

    added more features and refactores

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import . "github.com/bilgihankose/golang-bookstore/handlers"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/bilgihankose/golang-bookstore/handlers"
+	. "github.com/bilgihankose/golang-bookstore/utils"
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
+	"log"
+	"os"
+)
 
-func main(){
- Run()
+var db *sql.DB
+
+func init() {
+	CheckError(gotenv.Load())
+
+}
+
+func main() {
+
+	pgURL, err := pq.ParseURL(os.Getenv("POSTGRESQL_URL"))
+	CheckError(err)
+	fmt.Println("URL is", pgURL)
+
+	db, err = sql.Open("postgres", pgURL)
+	err = db.Ping() // Need to do this to check that the connection is valid
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handlers.Run()
+
 }
--- handlers/handler.go
+++ handlers/handler.go
@@ -1,13 +1,20 @@
 package handlers
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	. "github.com/bilgihankose/golang-bookstore/models"
 	. "github.com/bilgihankose/golang-bookstore/utils"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-func Run(){
+var books []Book
+var db *sql.DB
+
+func Run() {
 	fmt.Println("Running server on :8080")
 
 	router := mux.NewRouter()
@@ -22,23 +29,51 @@ func Run(){
 
 }
 
+func getAllBooks(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	books = []Book{}
 
-func getAllBooks(w http.ResponseWriter, r *http.Request){
-	fmt.Println("getAllBooks is called")
+	rows, err := db.Query("select * from books")
+	fmt.Println(err)
+
+	for rows.Next() {
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		fmt.Println(err)
+		books = append(books, book)
+	}
+	json.NewEncoder(w).Encode(books)
+
+	defer rows.Close()
 }
 
-func getBook(w http.ResponseWriter, r *http.Request){
-	fmt.Println("getBook is called")
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+	var book Book
+	params := mux.Vars(r)
+
+	rows := db.QueryRow("select * from books where id=$1", params["id"])
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	log.Fatal(err)
+
+	json.NewEncoder(w).Encode(book)
 }
 
-func addBooks(w http.ResponseWriter, r *http.Request){
-	fmt.Println("addBooks is called")
+func addBooks(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	var bookID int
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	err := db.QueryRow("insert into books (title, author, year) values ($1, $2, $3) RETURNING id;", book.Title, book.Author, book.Year).Scan(&bookID)
+	log.Fatal(err)
+
+	json.NewEncoder(w).Encode(bookID)
 }
 
-func updateBooks(w http.ResponseWriter, r *http.Request){
+func updateBooks(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("updateBooks is called")
 }
 
-func removeBook(w http.ResponseWriter, r *http.Request){
+func removeBook(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("removeBook is called")
 }
